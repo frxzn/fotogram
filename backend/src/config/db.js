79570fbea3b0a7127ae0c1449dcedac3fdd05e69
frxzn <hamdionline166@gmail.com>
2025-07-23 11:38:47 +0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // Artık Mongoose 6+ ile varsayılan, ama belirtmek zarar vermez
-            useUnifiedTopology: true, // Artık Mongoose 6+ ile varsayılan
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
         console.log(`MongoDB Bağlantısı Başarılı: ${conn.connection.host}`);
     } catch (error) {
         console.error(`MongoDB Bağlantı Hatası: ${error.message}`);
-        process.exit(1); // Uygulamayı hata ile kapat
+        process.exit(1);
     }
 };
 
