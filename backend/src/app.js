2171e3906061c1bbdc4b21e82cdd5e76d097fe5f
frxzn <hamdionline166@gmail.com>
2025-07-25 05:26:54 +0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,3 +43,8 @@ app.all('*', (req, res, next) => {
 });
 
 module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda çalışıyor`);
+});
